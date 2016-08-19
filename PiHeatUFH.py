5f120c545b0de32e48720080d3029dbdcad805d1
rmedhurs <rmedhurs@cisco>
2016-08-19 13:04:26 +0100

    Prototype of API

--- PiHeatUFH.py
+++ PiHeatUFH.py
@@ -6,9 +6,9 @@ from flask import Flask, jsonify, request
 from sensors import *
 
 #from decorators import crossdomain
-
-app = Flask(__name__)
-app.debug = True
+try:
+    app = Flask(__name__)
+    app.debug = True
 
 # app.secret_key = 'my_#$%^&_security_&*(4_key'
 
@@ -72,167 +72,170 @@ app.debug = True
 #             'error': 'Auth requerided'}
 #     return jsonify(data)
 
-VALID_BCM_PIN_NUMBERS = [15, 17, 18, 27, 22]
-VALID_HIGH_VALUES = [1, '1', 'HIGH']
-VALID_LOW_VALUES = [0, '0', 'LOW']
-PIN_NAMES = {'15': 'IN1',
-             '17': 'IN2',
-             '18': 'IN3',
-             '27': 'IN4',
-             '22': 'IN5'}  # ,
-#             '24': 'IN6',
-#             '25': 'IN7',
-#             '4': 'IN8'}
+    VALID_BCM_PIN_NUMBERS = [15, 17, 18, 27, 22]
+    VALID_HIGH_VALUES = [1, '1', 'HIGH']
+    VALID_LOW_VALUES = [0, '0', 'LOW']
+    PIN_NAMES = {'15': 'IN1',
+                 '17': 'IN2',
+                 '18': 'IN3',
+                 '27': 'IN4',
+                 '22': 'IN5'}  # ,
+    #             '24': 'IN6',
+    #             '25': 'IN7',
+    #             '4': 'IN8'}
 
-GPIO.setmode(GPIO.BCM)
+    GPIO.setmode(GPIO.BCM)
 
-for pin in VALID_BCM_PIN_NUMBERS:
-    GPIO.setup(pin, GPIO.OUT)
-
-
-def pin_status(pin_number):
-    if pin_number in VALID_BCM_PIN_NUMBERS:
-        value = GPIO.input(pin_number)
-        data = {'pin_number': pin_number,
-                'pin_name': PIN_NAMES[str(pin_number)],
-                'value': value,
-                'status': 'SUCCESS',
-                'error': None}
-    else:
-        data = {'status': 'ERROR',
-                'error': 'Invalid pin number.'}
-
-    return data
+    for pin in VALID_BCM_PIN_NUMBERS:
+        GPIO.setup(pin, GPIO.OUT)
 
 
-def pin_update(pin_number, value):
-    if pin_number in VALID_BCM_PIN_NUMBERS:
-        GPIO.output(pin_number, value)
-        new_value = GPIO.input(pin_number)
-        data = {'status': 'SUCCESS',
-                'error': None,
-                'pin_number': pin_number,
-                'pin_name': PIN_NAMES[str(pin_number)],
-                'new_value': new_value}
-    else:
-        data = {'status': 'ERROR',
-                'error': 'Invalid pin number or value.'}
+    def pin_status(pin_number):
+        if pin_number in VALID_BCM_PIN_NUMBERS:
+            value = GPIO.input(pin_number)
+            data = {'pin_number': pin_number,
+                    'pin_name': PIN_NAMES[str(pin_number)],
+                    'value': value,
+                    'status': 'SUCCESS',
+                    'error': None}
+        else:
+            data = {'status': 'ERROR',
+                    'error': 'Invalid pin number.'}
 
-    return data
+        return data
 
 
-@app.route("/api/v1/ping/", methods=['GET'])
-#@crossdomain(origin='*')
-def api_status():
-    if request.method == 'GET':
-        data = {'api_name': 'RPi GPIO API',
-                'version': '1.0',
-                'status': 'SUCCESS',
-                'response': 'pong'}
+    def pin_update(pin_number, value):
+        if pin_number in VALID_BCM_PIN_NUMBERS:
+            GPIO.output(pin_number, value)
+            new_value = GPIO.input(pin_number)
+            data = {'status': 'SUCCESS',
+                    'error': None,
+                    'pin_number': pin_number,
+                    'pin_name': PIN_NAMES[str(pin_number)],
+                    'new_value': new_value}
+        else:
+            data = {'status': 'ERROR',
+                    'error': 'Invalid pin number or value.'}
+
+        return data
+
+
+    @app.route("/api/v1/ping/", methods=['GET'])
+    #@crossdomain(origin='*')
+    def api_status():
+        if request.method == 'GET':
+            data = {'api_name': 'RPi GPIO API',
+                    'version': '1.0',
+                    'status': 'SUCCESS',
+                    'response': 'pong'}
+            return jsonify(data)
+
+
+    @app.route("/api/v1/gpio/<pin_number>/", methods=['POST', 'GET'])
+    #@crossdomain(origin='*')
+    #@login_required
+    def gpio_pin(pin_number):
+        pin_number = int(pin_number)
+        if request.method == 'GET':
+            data = pin_status(pin_number)
+
+        elif request.method == 'POST':
+            value = request.values['value']
+            if value in VALID_HIGH_VALUES:
+                data = pin_update(pin_number, 1)
+            elif value in VALID_LOW_VALUES:
+                data = pin_update(pin_number, 0)
+            else:
+                data = {'status': 'ERROR',
+                        'error': 'Invalid value.'}
         return jsonify(data)
 
 
-@app.route("/api/v1/gpio/<pin_number>/", methods=['POST', 'GET'])
-#@crossdomain(origin='*')
-#@login_required
-def gpio_pin(pin_number):
-    pin_number = int(pin_number)
-    if request.method == 'GET':
-        data = pin_status(pin_number)
+    @app.route("/api/v1/gpio/status/", methods=['GET'])
+    #@crossdomain(origin='*')
+    def gpio_status():
+        data_list = []
+        for pin in VALID_BCM_PIN_NUMBERS:
+            data_list.append(pin_status(pin))
 
-    elif request.method == 'POST':
-        value = request.values['value']
-        if value in VALID_HIGH_VALUES:
-            data = pin_update(pin_number, 1)
-        elif value in VALID_LOW_VALUES:
-            data = pin_update(pin_number, 0)
-        else:
-            data = {'status': 'ERROR',
-                    'error': 'Invalid value.'}
-    return jsonify(data)
+        data = {'data': data_list}
+        return jsonify(data)
 
 
-@app.route("/api/v1/gpio/status/", methods=['GET'])
-#@crossdomain(origin='*')
-def gpio_status():
-    data_list = []
-    for pin in VALID_BCM_PIN_NUMBERS:
-        data_list.append(pin_status(pin))
+    @app.route("/api/v1/gpio/all-high/", methods=['POST'])
+    #@crossdomain(origin='*')
+    def gpio_all_high():
+        data_list = []
+        for pin in VALID_BCM_PIN_NUMBERS:
+            data_list.append(pin_update(pin, 1))
 
-    data = {'data': data_list}
-    return jsonify(data)
-
-
-@app.route("/api/v1/gpio/all-high/", methods=['POST'])
-#@crossdomain(origin='*')
-def gpio_all_high():
-    data_list = []
-    for pin in VALID_BCM_PIN_NUMBERS:
-        data_list.append(pin_update(pin, 1))
-
-    data = {'data': data_list}
-    return jsonify(data)
+        data = {'data': data_list}
+        return jsonify(data)
 
 
-@app.route("/api/v1/gpio/all-low/", methods=['POST'])
-#@crossdomain(origin='*')
-def gpio_all_low():
-    data_list = []
-    for pin in VALID_BCM_PIN_NUMBERS:
-        data_list.append(pin_update(pin, 0))
+    @app.route("/api/v1/gpio/all-low/", methods=['POST'])
+    #@crossdomain(origin='*')
+    def gpio_all_low():
+        data_list = []
+        for pin in VALID_BCM_PIN_NUMBERS:
+            data_list.append(pin_update(pin, 0))
 
-    data = {'data': data_list}
-    return jsonify(data)
+        data = {'data': data_list}
+        return jsonify(data)
 
 
-# Set of REST methonds to read sensor values
-@app.route("/api/v1/sensors/temperature/status/", methods=['GET'])
-#@crossdomain(origin='*')
-def sensor_status():
-    data_list = []
-    TemperatureSensors = W1ThermSensor()
-    # scommand = input("Press any key to continue ")
-    # sensors = TemperatureSensors.get_available_sensors()
-    for s in TemperatureSensors.get_available_sensors():
-        data_list.append(temp_sensor_status(s))
-       # print(s.id + " " + s.type_name + " " + str(temp) + " " + str(round(temp, 1)) + "C")
+    # Set of REST methonds to read sensor values
+    @app.route("/api/v1/sensors/temperature/status/", methods=['GET'])
+    #@crossdomain(origin='*')
+    def sensor_status():
+        data_list = []
+        TemperatureSensors = W1ThermSensor()
+        # scommand = input("Press any key to continue ")
+        # sensors = TemperatureSensors.get_available_sensors()
+        for s in TemperatureSensors.get_available_sensors():
+            data_list.append(temp_sensor_status(s))
+            # print(s.id + " " + s.type_name + " " + str(temp) + " " + str(round(temp, 1)) + "C")
 
-    #for pin in VALID_BCM_PIN_NUMBERS:
-    #    data_list.append(pin_status(pin))
+        #for pin in VALID_BCM_PIN_NUMBERS:
+        #    data_list.append(pin_status(pin))
 
-    data = {'data': data_list}
-    return jsonify(data)
+        data = {'data': data_list}
+        return jsonify(data)
 
-def temp_sensor_status (temp_sensor):
-    try:
-        data = {'sensor_id': temp_sensor.id,
-                'sensor_name': temp_sensor.get_name(),  
-                'sensor_type': temp_sensor.type_name,
-                'temperature': str(round(temp_sensor.get_temperature(),1))+"C",
-                'status': 'SUCCESS',
-                'error': None}
-        return data
-    except UnsupportedUnitError:
-        raise UnsupportedUnitError()
-    except NoSensorFoundError:
-        error="banana"
-        data = {'sensor_id': error,
-                'sensor_name': error,
-                'sensor_type': error,
-                'temperature': error,
-                'status': 'SUCCESS',
-                'error': None}
-        return data
-    except SensorNotReadyError:
-        error="apple"
-        data = {'sensor_id': temp_sensor.id,
-                'sensor_name': temp_sensor.get_name(),
-                'sensor_type': temp_sensor.type_name,
-                'temperature': None,
-                'status': 'FAILURE',
-                'error': 'SensorNotReady'}
-        return data
+    def temp_sensor_status (temp_sensor):
+        try:
+            data = {'sensor_id': temp_sensor.id,
+                    'sensor_name': temp_sensor.get_name(),
+                    'sensor_type': temp_sensor.type_name,
+                    'temperature': str(round(temp_sensor.get_temperature(),1))+"C",
+                    'status': 'SUCCESS',
+                    'error': None}
+            return data
+        except UnsupportedUnitError:
+            raise UnsupportedUnitError()
+        except NoSensorFoundError:
+            error="banana"
+            data = {'sensor_id': error,
+                    'sensor_name': error,
+                    'sensor_type': error,
+                    'temperature': error,
+                    'status': 'SUCCESS',
+                    'error': None}
+            return data
+        except SensorNotReadyError:
+            error="apple"
+            data = {'sensor_id': temp_sensor.id,
+                    'sensor_name': temp_sensor.get_name(),
+                    'sensor_type': temp_sensor.type_name,
+                    'temperature': None,
+                    'status': 'FAILURE',
+                    'error': 'SensorNotReady'}
+            return data
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True)
+    if __name__ == "__main__":
+        app.run(host='0.0.0.0', port=80, debug=True)
 
+except KeyboardInterrupt:
+    print "Exiting"
+    GPIO.cleanup()
