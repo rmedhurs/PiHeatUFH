7cc9796dfd738c541dd2078f5a33b3c4c5cd5978
rmedhurs <rmedhurs@users.noreply.github.com>
2016-09-06 11:01:29 +0100

    Update my_database.py

--- my_database.py
+++ my_database.py
@@ -1,7 +1,9 @@
 def db_map(id):
-    if id == "0000078118d9":
+    if id == "0416588291ff":
         return "Return"
-    elif id == "000007811d65":
+    elif id == "03165563f2ff":
         return "Flow"
+    elif id == "03165528bcff"
+        return "Manifold"
     else:
         return "Unknown"
